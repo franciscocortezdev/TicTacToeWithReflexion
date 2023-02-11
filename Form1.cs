f45c66103787543d567764c3ed1fd7d7f05617a5
Francisco Cortez <franciscocortezdev@gmail.com>
2023-02-11 10:17:11 -0600

    Reflexion applied

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace TicTacToe
 {
     public partial class Form1 : Form
@@ -7,27 +9,101 @@ namespace TicTacToe
 
         void EnableFalse()
         {
-            btnTic1.Enabled = false;
-            btnTic2.Enabled = false;
-            btnTic3.Enabled = false;
-            btnTic4.Enabled = false;
-            btnTic5.Enabled = false;
-            btnTic6.Enabled = false;
-            btnTic7.Enabled = false;
-            btnTic8.Enabled = false;
-            btnTic9.Enabled = false;
+            for (int i = 1; i<10; i++)
+            {
+                FieldInfo myField = this.GetType().GetField($"btnTic{i}");
+                Button myButton = (Button)myField.GetValue(this);
+                myButton.Enabled = false;
+            }
 
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTic1_Click(object sender, EventArgs e)
+        {
+            var btnSelected = (Button)sender;
+           
+            if (checker)
+            {
+                btnSelected.Text = "O";
+                checker = false;
+            }
+            else
+            {
+                btnSelected.Text = "X";
+                checker = true;
+            }
+
+            Score();
+
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            lblWinsPlayerX.Text = "0";
+            lblWinsPlayerO.Text = "0";
+        }
+
+        private void btnResetGame_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
+
+        private void btnExitGame_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult DExit;
+                DExit = MessageBox.Show("Confirm if you want to exit", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (DExit == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+        }
+
+        public void ResetValues()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                FieldInfo myField = this.GetType().GetField($"btnTic{i}");
+
+                Button myButton = (Button)myField.GetValue(this);
+
+                myButton.Enabled = true;
+                myButton.Text = "";
+                myButton.BackColor = Color.White;
+            }
         }
 
         void Score()
         {
-            if(btnTic1.Text == "X" && btnTic2.Text == "X" && btnTic3.Text == "X")
+            if (btnTic1.Text == "X" && btnTic2.Text == "X" && btnTic3.Text == "X")
             {
                 btnTic1.BackColor = Color.PowderBlue;
                 btnTic2.BackColor = Color.PowderBlue;
                 btnTic3.BackColor = Color.PowderBlue;
 
-                MessageBox.Show("The winner is player X","TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("The winner is player X", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 plusOne = int.Parse(lblWinsPlayerX.Text);
                 plusOne++;
                 lblWinsPlayerX.Text = Convert.ToString(plusOne);
@@ -225,130 +301,6 @@ namespace TicTacToe
 
 
 
-        }
-
-
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnTic1_Click(object sender, EventArgs e)
-        {
-
-            var btnSelected = (Button)sender;
-
-            if (checker)
-            {
-                btnSelected.Text = "O";
-                checker = false;
-            }
-            else
-            {
-                btnSelected.Text = "X";
-                checker = true;
-            }
-
-            Score();
-
-        }
-
-        private void btnNewGame_Click(object sender, EventArgs e)
-        {
-            btnTic1.Enabled = true;
-            btnTic2.Enabled = true;
-            btnTic3.Enabled = true;
-            btnTic4.Enabled = true;
-            btnTic5.Enabled = true;
-            btnTic6.Enabled = true;
-            btnTic7.Enabled = true;
-            btnTic8.Enabled = true;
-            btnTic9.Enabled = true;
-
-            btnTic1.Text = "";
-            btnTic2.Text = "";
-            btnTic3.Text = "";
-            btnTic4.Text = "";
-            btnTic5.Text = "";
-            btnTic6.Text = "";
-            btnTic7.Text = "";
-            btnTic8.Text = "";
-            btnTic9.Text = "";
-
-            lblWinsPlayerX.Text = "0";
-            lblWinsPlayerO.Text = "0";
-
-            btnTic1.BackColor = Color.White;
-            btnTic2.BackColor = Color.White;
-            btnTic3.BackColor = Color.White;
-            btnTic4.BackColor = Color.White;
-            btnTic5.BackColor = Color.White;
-            btnTic6.BackColor = Color.White;
-            btnTic7.BackColor = Color.White;
-            btnTic8.BackColor = Color.White;
-            btnTic9.BackColor = Color.White;
-
-        }
-
-        private void btnResetGame_Click(object sender, EventArgs e)
-        {
-            btnTic1.Enabled = true;
-            btnTic2.Enabled = true;
-            btnTic3.Enabled = true;
-            btnTic4.Enabled = true;
-            btnTic5.Enabled = true;
-            btnTic6.Enabled = true;
-            btnTic7.Enabled = true;
-            btnTic8.Enabled = true;
-            btnTic9.Enabled = true;
-
-            btnTic1.Text = "";
-            btnTic2.Text = "";
-            btnTic3.Text = "";
-            btnTic4.Text = "";
-            btnTic5.Text = "";
-            btnTic6.Text = "";
-            btnTic7.Text = "";
-            btnTic8.Text = "";
-            btnTic9.Text = "";
-
-
-            btnTic1.BackColor = Color.White;
-            btnTic2.BackColor = Color.White;
-            btnTic3.BackColor = Color.White;
-            btnTic4.BackColor = Color.White;
-            btnTic5.BackColor = Color.White;
-            btnTic6.BackColor = Color.White;
-            btnTic7.BackColor = Color.White;
-            btnTic8.BackColor = Color.White;
-            btnTic9.BackColor = Color.White;
-
-        }
-
-        private void btnExitGame_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                DialogResult DExit;
-                DExit = MessageBox.Show("Confirm if you want to exit", "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                if( DExit == DialogResult.Yes)
-                {
-                    Application.Exit();
-                }
-
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, "TicTacToe", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            }
         }
     }
 }
